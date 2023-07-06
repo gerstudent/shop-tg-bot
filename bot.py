93f515d2a2d153746f9ea86b1101730ffe7fb924
gerstudent <gkolcherin@gmail.com>
2023-07-06 16:32:57 +0300

    update

--- bot.py
+++ bot.py
@@ -9,12 +9,24 @@ dp = Dispatcher(bot=bot)
 
 @dp.message_handler(commands=['start'])
 async def cmd_start(message: types.Message):
+    await message.answer_sticker("CAACAgIAAxkBAAMGZKa_4SN7y425QV79kUw7GfMtYr0AAkIQAAIzxSlJkA7UEacqSoIvBA")
     await message.answer(f'{message.from_user.first_name}, добро пожаловать в магазин!')
 
 
 @dp.message_handler()
 async def answer(message: types.Message):
     await message.reply('Такой команды не существует')
+
+
+@dp.message_handler(content_types=['sticker'])
+async def check_stick(message: types.Message):
+    await message.answer(message.sticker.file_id)
+    await bot.send_message(message.from_user.id, message.chat.id)
+
+
+@dp.message_handler(content_types=['photo', 'document'])
+async def forward_file(message: types.Message):
+    await bot.forward_message(os.getenv("GROUP_ID"), message.from_user.id, message.message_id)
 
 
 if __name__ == '__main__':
